a5bda5e233864aa488dd7a49132640ca5dee49a3
nfv <nicklas@davik.dk>
2015-10-22 21:00:36 +0200

    Changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+var NFV = NFV|| {};
+NFV.OXID = NFV.OXID || {};
+var lol ={};
+$(document).ready(function () {
+  var cooks = NFV.OXID.Cookie();
+  lol =cooks;
+  cooks.init({});
+
+});
--- app/scripts/oxid.cookie.js
+++ app/scripts/oxid.cookie.js
@@ -2,7 +2,7 @@
  * Created by Nicklas.Vind on 18-10-2015.
  */
 
-  var config = {
+  var xconfig = {
     cookies:[
       {
         hostnames: ['www.oxid.io','www.oxid.dk'],
@@ -10,6 +10,11 @@
         headline:'headline',
         information:'text',
         enabled:true,
+        CookieConfig:{
+          saveAnswer: true,
+          cookieName:'OxidCookie',
+          saveAnswerPeriod:90
+        },
         buttons:[
           {
             button:'cookies-accept',
@@ -33,62 +38,154 @@
       }
     ]
   };
-var NFV = NFV|| {};
-NFV.OXID = NFV.OXID |{};
+var NFV = NFV || {};
+NFV.OXID = NFV.OXID || {};
 (function (module, $) {
   module.Cookie = (function () {
-    var init = function (config) {
-      for(var i = 0;config.cookies; i++){
-        var modal = config.cookies[i];
-        if(_hasDomain(modal.hostnames) && modal.enabled === true){
-          _enabledButtons(modal);
-          $(modal.cookieContainer).on('click', function (event) {
-            _clickEventsHandler(event);
+    var config = {};
+    var init = function (configuration) {
+
+      var _config = _fillBlankConfiguration(configuration);
+      for(var i = 0;i< _config.cookies.length; i++){
+        config = _config.cookies[i];
+        if(_hasDomain(config.hostnames) && config.enabled === true){
+          _enabledButtons(config);
+          _createCookie(config,false,window.location.hostname);
+          $(config.cookieContainer).on('click', function (event) {
+            event.preventDefault();
+            _clickEventsHandler(event, config.buttons);
           });
+          _cookieModal(config);
         }
       }
+      if(_getCookie() === undefined || false){
+       // show
+      }
+    };
+    var _fillBlankConfiguration = function (configs) {
+      var _config = {
+        cookies:[
+          {
+            hostnames: configs.hostnames || [],
+            cookieContainer:configs.cookieContainer || '#Oxidcookie',
+            headline:configs.headline || 'headline',
+            information:configs.information || 'text',
+            enabled:configs.enabled || true,
+            cookieConfig:configs.cookieConfig || {
+              saveAnswer:configs.saveAnswer || true,
+              cookieName:configs.cookieName || 'OxidCookie',
+              saveAnswerPeriod:configs.saveAnswerPeriod || 90
+            },
+            buttons:config.buttons || [
+              {
+                buttonId:'cookies-accept',
+                text:'Thanks!',
+                link: 'http://www.m.dk',
+                enabled: true
+              },
+              {
+                buttonId:'cookies-decline',
+                text:'No thanks!',
+                link:'http//www.m.dk',
+                enabled:false
+              },
+              {
+                buttonId:'more-info',
+                text:'read more',
+                link:'http//www.m.dk',
+                enabled:true
+              }
+            ]
+          }
+        ]
+      };
+      return _config;
+    };
+    var _createCookie = function (modal,answer, domain) {
+      Cookies.set(modal.cookieConfig.cookieName, answer,{
+        expires:config.cookieConfig.saveAnswerPeriod,
+        //  path:window.location.pathname,
+        domain:domain
+      });
+    };
+    var _updateCookie = function (answer) {
+      var _cook = Cookies.get(config.cookieConfig.cookieName);
+      _createCookie(answer, _cook.domain);
+    };
+    var _getCookie = function () {
+      var _cook = Cookies.get(config.cookieConfig.cookieName);
+      return _cook;
     };
     var _cookieModal = function (modal) {
       var _container = $(modal.cookieContainer);
-      _container.show();
       _container.find('emphasized-text').text = modal.headline;
       _container.find('cookie-text').text = modal.information;
+      _container.show();
     };
     var _hasDomain = function (domains) {
       var validDomain = false;
-      if(domains.indexOf(window.location.hostname) !== -1){
+      if(domains.indexOf(window.location.hostname) !== -1 || domains.length === 0){
         validDomain = true;
       }
       return validDomain;
     };
     var _enabledButtons = function (config) {
       var btns = config.buttons;
-      for(var i = 0; btns.length < i; i++){
+      for(var i = 0;btns.length > i; i++){
         var btn = btns[i];
         if(btn.enabled === true){
           _genericButton(btn);
         }
       }
     };
-    var _clickEventsHandler = function (evt) {
-      var btn =  $(evt.target);
-      if(buttons.indexOf(btn).attr('id')){
-        evt.preventDefault();
-        var url = btn.attr('href');
-        window.open(url, '_blank');
+    var _findobject = function (key, array) {
+      var id = 0,
+          obj = {};
+      for(var i = 0;array.length > i; ++i){
+        console.log(array[i]);
+       if(array[i].buttonId == key){
+         id = i;
+         obj = array[i];
+       }else{
+         id = -1;
+         obj = {};
+       }
       }
+      return {
+        id:id,
+        obj:obj
+      };
+    };
+    var _clickEventsHandler = function (evt, buttons) {
+      var btn =  $(evt.target);
+      var obj = _findobject(evt.target.id, buttons);
+      console.log(obj);
+      if(obj.id === evt.target.id && obj.obj.enabled === true){
+        if(btn.attr('id') === buttons[0].button){
+          _updateCookie(true);
+        }
+        var url = evt.href;
+
+        window.open(url);
+
+      }
+      console.log(buttons.indexOf(evt.target.id));
     };
       var _genericButton = function (options) {
-      var acceptBtn = $('#'+options.id);
+      var acceptBtn = $('#'+options.buttonId);
       if(options.enabled === true){
+        acceptBtn.html(options.text);
+        acceptBtn.attr('href',options.link);
+        acceptBtn.attr('target','_blank');
         acceptBtn.show();
-        acceptBtn.txt = options.text;
-        acceptBtn.href = options.link;
       }else{
         acceptBtn.hide();
       }
     };
     var publicApi = {
+      init:init,
+      config:config,
+      createCnfg:_fillBlankConfiguration
     };
     return publicApi;
   });
